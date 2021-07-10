52ee6540af29291ccd013b66db4fc154d4db3190
JustTeRoR <pavlenok2000@gmail.com>
2021-07-10 17:50:08 +0300

    Initial Commit

---
+++ Task Magician/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task Magician
+//
+//  Created by Сергей Павленок on 10.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
